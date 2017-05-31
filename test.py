5e9ebcf01375357ceb36569861f23ead7faba036
LoganW94 <loganwogoman@gmail.com>
2017-05-31 00:19:02 -0400

    fiddled

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ print(usr_home)
 
 path = os.path.join(usr_home, "Documents")
 print(path)
-subprocess.Popen(r'explorer /select, "%s" ' & path)
+#subprocess.Popen(r'explorer /select, "%s" ' & path)
 
